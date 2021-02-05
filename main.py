e2c80f00c6553e6be1aa3a30bb68dcbe6e9afb5e
BostonSlettum22 <816661@mystma.org>
2021-02-05 03:09:32 +0000

    letterpost 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+def letterPos(w,n):
+    letter = w[len(w)-1]
+
+    for i in range(0,len(w)+1):
+        letter = letter + w[len(w)-1]
+    if w == [len(n)-1]:
+        return len(w)
+    else:
+        return w[0]
+
+
+print(letterPos("dunder","d"))
